6cc91ae3a32f6a4480c6a42633b19a5ad4c6a415
Chalkki <946921362@qq.com>
2022-12-08 15:43:00 -0500

    add jump and roll(dash) for player

--- Dragon_Warrior_Unity/Assets/Actual Game/Scripts/Player.cs
+++ Dragon_Warrior_Unity/Assets/Actual Game/Scripts/Player.cs
@@ -8,7 +8,9 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float moveForce = 10f;
     [SerializeField]
-    private float jumpForce = 5f;
+    private float jumpForce = 10f;
+    [SerializeField]
+    private float rollForce = 15f;
     // Start is called before the first frame update
 
     private Animator anim;
@@ -17,7 +19,12 @@ public class Player : MonoBehaviour
 
     //the parameter for the character to run
     private string Run_Anim = "is_run";
-
+    private string Roll_Anim = "is_roll";
+    private string Jump_Anim = "jumping";
+    private bool canRoll = true;
+    private bool isRolling = false;
+    private float rollCooldown = 1f;
+    private float rollingTime = 0.2f;
 
     private void Awake()
     {
@@ -33,8 +40,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isRolling)
+        {
+            return;
+        }
         PlayerMoveKeyboard();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canRoll)
+        {
+            StartCoroutine(PlayerRoll());
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerJump();
+        }
     }
+
 
 
     void PlayerMoveKeyboard()
@@ -45,23 +67,46 @@ public class Player : MonoBehaviour
         if(movementX > 0)
         {
             // move right
-            anim.SetBool("is_run", true);
+            anim.SetBool(Run_Anim, true);
             sr.flipX = false;
         }else if (movementX < 0)
         {
             // move left
-            anim.SetBool("is_run", true);
+            anim.SetBool(Run_Anim, true);
             sr.flipX = true;
         }
         else
         {
             // stable
 
-            anim.SetBool("is_run", false);
+            anim.SetBool(Run_Anim, false);
         }
     }
     void PlayerJump()
     {
+        myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        anim.SetTrigger(Jump_Anim);
+    }
+
+    private IEnumerator PlayerRoll()
+    {
+        canRoll = false;
+        isRolling = true;
+        anim.SetBool("is_roll", true);
+        if (!sr.flipX)
+        {
+            myBody.velocity = new Vector2(transform.localScale.x * rollForce, 0f);
+        }
+        else
+        {
+            myBody.velocity = new Vector2(-transform.localScale.x * rollForce, 0f);
+        }
+        yield return new WaitForSeconds(rollingTime);
+        myBody.velocity = new Vector2(0f, 0f);
+        anim.SetBool("is_roll", false);
+        isRolling = false;
+        yield return new WaitForSeconds(rollCooldown);
+        canRoll = true;
 
     }
 }
