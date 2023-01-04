7f1262700f881f7d884fadffb2a30e22be82fcb4
Chalkki <946921362@qq.com>
2023-01-04 15:27:34 -0500

    the health of player can be displayed now

--- Dragon_Warrior_Unity/Assets/Actual Game/Scripts/PlayerHealth.cs
+++ Dragon_Warrior_Unity/Assets/Actual Game/Scripts/PlayerHealth.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public float maxHealth = 100;
+    public Slider slider;
+    float currentHealth;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        SetMaxHealth(maxHealth);
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void TakeDamage(float damage)
+    {
+        if(currentHealth<= 0)
+        {
+            return;
+        }
+        currentHealth -= damage;
+        if(currentHealth <= 0) {
+            SetHealth(0f);
+            Die();
+            return;
+        }
+        SetHealth(currentHealth);
+    }
+
+    void Die()
+    {
+        animator.SetTrigger("is_dead");
+        MonoBehaviour[] monos = GetComponentsInChildren<MonoBehaviour>();
+        foreach (MonoBehaviour mono in monos)
+        {
+            mono.enabled = false;
+        }
     }
 }
