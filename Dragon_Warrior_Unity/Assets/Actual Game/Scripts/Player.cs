ae7aab1ab59bc13b806b849a241d1980e9a4d88d
weizhengliang <lwz12@outlook.com>
2023-01-06 21:25:01 +0800

    edited the New Bee Valley scene, edited some player movement action

--- Dragon_Warrior_Unity/Assets/Actual Game/Scripts/Player.cs
+++ Dragon_Warrior_Unity/Assets/Actual Game/Scripts/Player.cs
@@ -8,39 +8,52 @@ public class Player : MonoBehaviour
 {
     private float movementX;
     [SerializeField]
-    private float moveForce = 10f;
+    private float
+        moveForce,
+        jumpForce,
+        rollForce,
+        // damage reaction
+        repelForceX = 10f,
+        repelForceY = 10f;
     [SerializeField]
-    private float jumpForce = 10f;
-    [SerializeField]
-    private float rollForce = 15f;
-
-    private bool facingRight = true;
+    private bool
+        facingRight = true;
 
     // Start is called before the first frame update
 
     private Animator anim;
     private Rigidbody2D myBody;
+    private CapsuleCollider2D coll;
+    private SpriteRenderer sprite;
 
     //animator parameter
-    private string Run_Anim = "is_run";
-    private string Roll_Anim = "is_roll";
-    private string Jump_Anim = "jumping";
+    private string
+        Run_Anim = "is_run",
+        Roll_Anim = "is_roll",
+        Jump_Anim = "jumping";
     // parameters for rolling
-    private bool canRoll = true;
-    private bool isRolling = false;
-    private float rollCooldown = 1f;
-    private float rollingTime = 0.2f;
-    // paremeters for jumping
-    private bool onGround = true;
+    private bool
+        canRoll = true,
+        isRolling = false;
+    private float
+        rollCooldown = 1f,
+        rollingTime = 0.2f;
+    // layermask for ground jumping only
+    [SerializeField]
+    private LayerMask jumpableGround;
     // parameters for climbing
-    public bool isLadder = false;
-    public bool isClimbing = false;
-    private float vertical = 0f;
-    private float climbSpeed = 5f;
+    public bool
+        isLadder = false,
+        isClimbing = false;
+    private float
+        vertical = 0f,
+        climbSpeed = 5f;
     private void Awake()
     {
         anim = GetComponent<Animator>();
         myBody = GetComponent<Rigidbody2D>();
+        coll = GetComponent<CapsuleCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
     void Start()
     {
@@ -61,7 +74,7 @@ public class Player : MonoBehaviour
             StartCoroutine(PlayerRoll());
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && onGround == true)
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             PlayerJump();
         }
@@ -91,7 +104,12 @@ public class Player : MonoBehaviour
     void PlayerMoveKeyboard()
     {
         movementX = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(movementX, 0, 0) * moveForce * Time.deltaTime;
+        
+
+        if (myBody.bodyType == RigidbodyType2D.Dynamic)
+        {
+            myBody.velocity = new Vector2(movementX * moveForce, myBody.velocity.y);
+        }
 
         if(movementX > 0)
         {
@@ -100,7 +118,7 @@ public class Player : MonoBehaviour
             if (!facingRight)
             {
                 facingRight = true;
-                transform.Rotate(new Vector3(0f, 180f, 0f));
+                sprite.flipX = false;
             }
         }else if (movementX < 0)
         {
@@ -109,7 +127,7 @@ public class Player : MonoBehaviour
             if (facingRight)
             {
                 facingRight = false;
-                transform.Rotate(new Vector3(0f, 180f, 0f));
+                sprite.flipX = true;
             }
         }
         else
@@ -121,16 +139,41 @@ public class Player : MonoBehaviour
     }
     void PlayerJump()
     {
-        myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        myBody.velocity = new Vector2(myBody.velocity.x, jumpForce);
         anim.SetTrigger(Jump_Anim);
-        onGround = false;
+        
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.CapsuleCast(coll.bounds.center, coll.bounds.size, CapsuleDirection2D.Vertical, 0f, Vector2.down, .1f, jumpableGround); 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy"))
         {
-            onGround = true;
+
+            //Vector2 repelForceDirection = coll.bounds.center - collision.collider.bounds.center;
+            //if(repelForceDirection.x <= 0)
+            //{
+            //    // repel left
+            //    myBody.AddForce(new Vector2(-repelForceX, 0f), ForceMode2D.Force);
+            //}
+            //else
+            //{
+            //    // repel right
+            //myBody.AddForce(new Vector2(repelForceX, repelForceY), ForceMode2D.Force);
+            //}
+            if (myBody.bodyType == RigidbodyType2D.Dynamic)
+            {
+                myBody.AddForce(new Vector2(repelForceX, repelForceY), ForceMode2D.Impulse);
+            }
+            
+            //myBody.velocity = new Vector2(repelForceX, repelForceY);
+
+            Debug.Log(myBody.velocity);
+            
         }
     }
 
